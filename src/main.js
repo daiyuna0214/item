7f3b086dd8ccc8b2b3328c34aa978aab70c2089a
daiyuna0214 <2034892606@qq.com>
2019-12-20 15:54:12 +0800

    初始化项目准备完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './styles/index.less'// 引入初始化样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'// 引入样式
+Vue.use(ElementUI)// 注册
 
 Vue.config.productionTip = false
 
